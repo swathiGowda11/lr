b643eb8ac447394c7391bcbf4609a26e9f43a823
swathiGowda11 <151748189+swathiGowda11@users.noreply.github.com>
2025-12-05 10:20:54 +0530

    Remove streamlit installation command
    Removed pip install command for streamlit.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1heGm-DsId7bzzHtKMR61F5OagXzPvvyu
 """
 
-!pip install streamlit
+
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
